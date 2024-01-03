5a0d1013e61c669aaa1cc592bd50fa3d9222e90d
namvu <namvu61097@gmail.com>
2024-01-03 09:50:53 +0700

    [Add] NShop Project

---
+++ src/main/java/com/vti/nshop/NshopApplication.java
@@ -0,0 +1,13 @@
+package com.vti.nshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NshopApplication.class, args);
+	}
+
+}
